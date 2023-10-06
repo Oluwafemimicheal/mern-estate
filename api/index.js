a9daee49601c01ee9a57edc69bfe6916f0a8b5da
Oluwafemi Micheal <oluwafemicu@gmail>
2023-10-05 22:34:22 -0600

    create database

--- api/index.js
+++ api/index.js
@@ -1,14 +1,24 @@
 import express from 'express'
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const app = express()
 
-// app.get('/', (req, res)=>{
-
-// })
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('Connected to MongoDB')
+})
+.catch((err)=>{
+    console.log(err)
+});
 
 app.listen(3000, ()=>{
     console.log('Server running on port 3000')
 })
 
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
